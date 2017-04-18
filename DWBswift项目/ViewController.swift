d74dd4226b8416b5f7a88ed43b294ffea702ae9c
micheng23 <micheng8888@163.com>
2017-04-18 13:50:48 +0800

    第一次上传新项目

--- DWBswift项目/ViewController.swift
+++ DWBswift项目/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var titleArray : [String] = ["1", "2", "3", "4", "5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+           
+       print("我的github测试")
+        
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
